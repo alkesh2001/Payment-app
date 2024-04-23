5a3b75541d87b8b52eacf82f049063024aded07f
alkesh <alkesh9639@gmail.com>
2024-04-23 12:36:56 +0530

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+
+function App() {
+
+
+  return (
+    <>
+      <div className='bg-blue-400 p-3'>
+         <h1 className='bg-green-400 text-lg font-medium'>hello guyss this is react app</h1>
+      </div>
+
+    </>
+  )
+}
+
+export default App
